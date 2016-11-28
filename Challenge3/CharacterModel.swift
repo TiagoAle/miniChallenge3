3762f017766b95704a18b07009f239ef10d2ba2c
Daniel Dias <danieldias@macbook-pro-de-daniel.local>
2016-11-28 10:29:09 -0300

    model character iniciado

---
+++ Challenge3/CharacterModel.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterModel.swift
+//  Challenge3
+//
+//  Created by Daniel Dias on 28/11/16.
+//  Copyright © 2016 Tiago Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class CharacterModel: NSObject {
+    
+    var charImage: UIImage?
+    var nickName: String?
+    var age: Int?
+    var items: [String]?
+    var missions: [String]?
+
+    
+    init() {
+        <#code#>
+    }
+}
