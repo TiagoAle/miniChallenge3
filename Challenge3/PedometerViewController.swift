ae6419bb866ca38375c33acf5fbb912fe2387627
Tiago Queiroz <tiago.queiroz_2009@hotmail.com>
2016-11-30 08:54:20 -0300

    Init velocity

--- Challenge3/PedometerViewController.swift
+++ Challenge3/PedometerViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import CoreMotion
-class PedometerViewController: UIViewController {
+import CoreLocation
+
+class PedometerViewController: UIViewController, CLLocationManagerDelegate {
     
    // @IBOutlet weak var activityState: UILabel!
     @IBOutlet weak var steps: UILabel!
     
     var days:[String] = []
     var stepsTaken:[Int] = []
+    var manager = CLLocationManager()
     
    // @IBOutlet weak var stateImageView: UIImageView!
     let activityManager = CMMotionActivityManager()
@@ -23,6 +26,7 @@ class PedometerViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        self.manager.delegate = self
         
         var cal = Calendar.current
         var comps = cal.dateComponents([Calendar.Component.year,Calendar.Component.month, Calendar.Component.day, Calendar.Component.hour, Calendar.Component.minute, Calendar.Component.second], from: Date())
@@ -36,25 +40,22 @@ class PedometerViewController: UIViewController {
         let midnightOfToday = cal.date(from: comps)
         
         
-//        if(CMMotionActivityManager.isActivityAvailable()){
-//            self.activityManager.startActivityUpdates(to: OperationQueue.main, withHandler: { (data) -> Void in
-//                DispatchQueue.main.async(execute: { () -> Void in
-//                    if(data!.stationary == true){
-//                        self.activityState.text = "Stationary"
-//                        self.stateImageView.image = UIImage(named: "Sitting")
-//                    } else if (data!.walking == true){
-//                        self.activityState.text = "Walking"
-//                        self.stateImageView.image = UIImage(named: "Walking")
-//                    } else if (data!.running == true){
-//                        self.activityState.text = "Running"
-//                        self.stateImageView.image = UIImage(named: "Running")
-//                    } else if (data!.automotive == true){
-//                        self.activityState.text = "Automotive"
-//                    }
-//                })
-//                
-//            })
-//        }
+        if(CMMotionActivityManager.isActivityAvailable()){
+            self.activityManager.startActivityUpdates(to: OperationQueue.main, withHandler: { (data) -> Void in
+                DispatchQueue.main.async(execute: { () -> Void in
+                    if(data!.stationary == true){
+                        print("Stationary")
+                    } else if (data!.walking == true){
+                        print("Walking")
+                    } else if (data!.running == true){
+                        print("Running")
+                    } else if (data!.automotive == true){
+                        print("Automotive")
+                    }
+                })
+                
+            })
+        }
         if(CMPedometer.isStepCountingAvailable()){
             let fromDate = NSDate(timeIntervalSinceNow: -86400 * 7)
             self.pedoMeter.queryPedometerData(from: fromDate as Date, to: Date(), withHandler: { (data, error) in
@@ -83,5 +84,10 @@ class PedometerViewController: UIViewController {
         
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var lastLocation = locations.last
+        var secondLast = locations[locations.count - 2]
+        var speed: CLLocationSpeed = (lastLocation?.speed)! - secondLast.speed
+    }
     
 }
