2be7638e82cdad42c6203b18074be14e8b53b22c
Daniel Dias <danieldias@MacBook-Pro-de-Daniel.local>
2016-12-02 10:36:15 -0300

    copiado

---
+++ Challenge3/QuestTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  QuestTableViewCell.swift
+//  miniChallenge3
+//
+//  Created by Islas Girão Garcia on 01/12/16.
+//  Copyright © 2016 Islas Girão Garcia. All rights reserved.
+//
+
+import UIKit
+
+class QuestTableViewCell: UITableViewCell {
+
+    
+
+    @IBOutlet weak var questImage: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var reward: UILabel!
+    
+    @IBOutlet weak var questDescription: UILabel!
+    
+    @IBOutlet weak var buttonStartQuest: UIButton!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.title.text = "titulodaquest"
+        self.questImage.image = #imageLiteral(resourceName: "male")
+        self.reward.text = "tu perde peso"
+        self.questDescription.text = "bem facim man"
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
