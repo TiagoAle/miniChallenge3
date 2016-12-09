2a963e9ae3f25e491e505bda47d844e5927f13d4
Tiago Queiroz <tiago.queiroz_2009@hotmail.com>
2016-12-09 09:02:34 -0300

    FireBase begin

--- Challenge3/AppDelegate.swift
+++ Challenge3/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Firebase
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FIRApp.configure()
+        
+        FIRAuth.auth()?.signIn(withEmail: "gitmove@bepid.com", password: "git123", completion: {(user: FIRUser?, error: Error?) in
+            if error == nil{
+                print((user?.email)! as String)
+            }else{
+                print((error?.localizedDescription)! as String)
+            }
+            
+        })
         // Override point for customization after application launch.
         return true
     }
