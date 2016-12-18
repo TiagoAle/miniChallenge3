9186c792574184cb4eb554a3a345ded15669a2a7
Daniel Dias <danieldias@MacBook-Pro-de-Daniel.local>
2016-12-18 13:26:13 -0300

    cadastrando somente um usuário e não alterando por cima dele

--- Challenge3/RegisterViewController.swift
+++ Challenge3/RegisterViewController.swift
@@ -18,7 +18,7 @@ class RegisterViewController: UIViewController,UITextFieldDelegate, UserDataMana
     
     let dataManager = UserDataManager()
     var info: [Any]?
-
+    var test : Bool? = true//
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,15 +32,8 @@ class RegisterViewController: UIViewController,UITextFieldDelegate, UserDataMana
     @IBAction func registerAction(_ sender: UIButton) {
 
         
-        self.nickLabel.text = self.dataManager.saveNickName(nickName: self.nickTextField.text!)
-        //let dict = ["missão1" : "muito difícil", "gol": "Da Alemanha"]
-        let ref = FIRDatabase.database().reference(fromURL: "https://gitmove-e1481.firebaseio.com/")
-        ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["nick" : self.dataManager.nickName])
-        ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["age" : self.info?[0] as! Int])
-        ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["gender" : self.info?[1] as! String])
-        ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["exp" : 0])
-        ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["level" : 1])
-        //ref.child("CharacterModel").child(self.dataManager.nickName).child("missionsAvailable").childByAutoId().setValue(dict)
+        self.userExistDB(user: self.nickTextField.text!)
+        print(self.test)
         
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -62,6 +55,48 @@ class RegisterViewController: UIViewController,UITextFieldDelegate, UserDataMana
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    
+    func userExistDB(user: String?) {
+        
+        
+        if let nick = user {
+            
+            CharacterModel.asyncAll { (json) in
+                
+                var teste = false
+                
+                for key in json.keys{
+                    
+                    if nick == key{
+                        print(key)
+                        teste = true
+                    }
+                    
+                }
+                
+                self.test = teste
+                
+                if !self.test! {
+                    
+                    
+                    self.nickLabel.text = self.dataManager.saveNickName(nickName: self.nickTextField.text!)
+                    //let dict = ["missão1" : "muito difícil", "gol": "Da Alemanha"]
+                    let ref = FIRDatabase.database().reference(fromURL: "https://gitmove-e1481.firebaseio.com/")
+                    ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["nick" : self.dataManager.nickName])
+                    ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["age" : self.info?[0] as! Int])
+                    ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["gender" : self.info?[1] as! String])
+                    ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["exp" : 0])
+                    ref.child("CharacterModel").child(self.dataManager.nickName).updateChildValues(["level" : 1])
+                    
+                    print("o usuario esta sendo salvo")
+                    // seta alguma outra UX aqui
+                    
+                }
+            }
+        }
+    }
+
     
 
     /*
